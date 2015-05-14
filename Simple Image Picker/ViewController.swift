52a9d9ae31201aba5a1b7c4cff68668777b95b45
Jon Gordon <jon@jongor-software.com>
2015-05-14 19:44:25 +0100

    Button linked to ImagePicker- simple!

--- Simple Image Picker/ViewController.swift
+++ Simple Image Picker/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func experiment(sender: UIButton) {
+        let nextController = UIImagePickerController()
+        self.presentViewController(nextController, animated: true, completion: nil)
+    }
 
 }
 
